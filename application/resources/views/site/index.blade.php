10ebd32779fb7701244cc488abe6f5541c1ae66e
cy322666 <cy322666@gmail.com>
2022-09-14 14:40:38 +0200

    add amochat

--- application/resources/views/site/index.blade.php
+++ application/resources/views/site/index.blade.php
@@ -380,7 +380,7 @@
 
 {{--    amochat   --}}
     <script>
-        (function(a,m,o,c,r,m){a[m]={id:"288369",hash:"55c47ab8293eccbb80651c09b4012ca2c5bf3902811f84b3ac967e26cd756728",locale:"ru",inline:true,setMeta:function(p){this.params=(this.params||[]).concat([p])}};a[o]=a[o]||function(){(a[o].q=a[o].q||[]).push(arguments)};var d=a.document,s=d.createElement('script');s.async=true;s.id=m+'_script';s.src='https://gso.amocrm.ru/js/button.js?1663158646';d.head&&d.head.appendChild(s)}(window,0,'amoSocialButton',0,0,'amo_social_button'));
+        (function(a,m,o,c,r,m){a[m]={id:"290181",hash:"d0b625ed48b9390dbeca8ed9ac0b8f1e771dd8be82080abcbc7f4fa429ca7749",locale:"ru",inline:true,setMeta:function(p){this.params=(this.params||[]).concat([p])}};a[o]=a[o]||function(){(a[o].q=a[o].q||[]).push(arguments)};var d=a.document,s=d.createElement('script');s.async=true;s.id=m+'_script';s.src='https://gso.amocrm.ru/js/button.js?1663159216';d.head&&d.head.appendChild(s)}(window,0,'amoSocialButton',0,0,'amo_social_button'));
     </script>
 </body>
 
