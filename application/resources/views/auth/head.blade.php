e41cce2524f688d96313c786fb849fcad290a269
cy322666 <cy322666@gmail.com>
2022-09-14 15:21:58 +0200

    fix css

--- application/resources/views/auth/head.blade.php
+++ application/resources/views/auth/head.blade.php
@@ -8,10 +8,10 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="apple-touch-icon" sizes="76x76" href={{ asset("site/img/apple-icon.png") }}>
-    <link rel="icon" type="image/png" href={{ asset("site/img/favicon.png") }}>
+{{--    <link rel="apple-touch-icon" sizes="76x76" href={{ asset("site/img/apple-icon.png") }}>--}}
+{{--    <link rel="icon" type="image/png" href={{ asset("site/img/favicon.png") }}>--}}
 
-    <title>BLACK CLEVER</title>
+    <title>Black Clever</title>
 </head>
 
 <body class="sign-in-basic">
