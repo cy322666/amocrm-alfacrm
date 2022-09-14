c2d9f60e0a172046db88ef3b0f58ba8fb81558f1
cy322666 <cy322666@gmail.com>
2022-09-14 14:14:01 +0200

    fix redirect flow

--- application/app/Http/Controllers/Auth/amoCRMController.php
+++ application/app/Http/Controllers/Auth/amoCRMController.php
@@ -41,13 +41,13 @@ class amoCRMController extends Controller
         try {
             (new amoApi($account))->init();
 
-            redirect(route('account'), ['auth' => true]);
+            return redirect()->route('account', ['auth' => true]);
 
         } catch (\Throwable $exception) {
 
             Log::error(__METHOD__.' : '.$exception->getMessage());
 
-            return back()->withInput(['auth' => false]);
+            return redirect()->route('account', ['auth' => false]);
         }
     }
 
