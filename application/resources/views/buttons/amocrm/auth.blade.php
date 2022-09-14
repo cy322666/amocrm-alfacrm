8841e30837a626cd802904d345993ac348c3d314
cy322666 <cy322666@gmail.com>
2022-09-14 17:48:08 +0200

    fix install amocrm

--- application/resources/views/buttons/amocrm/auth.blade.php
+++ application/resources/views/buttons/amocrm/auth.blade.php
@@ -5,7 +5,7 @@
         data-name="{{ config('services.amocrm.app_name') }}"
         data-description="{{ config('services.amocrm.description') }}"
         data-redirect_uri="{{ config('services.amocrm.redirect_uri') }}"
-        data-secrets_uri="{{ config('services.amocrm.secrets_uri') }}.?account={{ \Illuminate\Support\Facades\Auth::user()->email }}"
+        data-secrets_uri="{{ config('services.amocrm.secrets_uri') }}?account={{ \Illuminate\Support\Facades\Auth::user()->email }}"
         data-logo=""
         data-scopes="crm"
         data-title="Подключить платформу"
