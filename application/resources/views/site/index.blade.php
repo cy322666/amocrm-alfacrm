fe62a5327cd87e950365c54d5af75de185102c88
cy322666 <cy322666@gmail.com>
2022-09-15 13:48:17 +0200

    add meta tags

--- application/resources/views/site/index.blade.php
+++ application/resources/views/site/index.blade.php
@@ -6,7 +6,9 @@
 
 <head>
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta property="og:title" content="Внедрение AmoCRM">
+    <meta property="og:description" content="Внедрение и настройка amoCRM">
+    <meta property="og:type" content="website">
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
