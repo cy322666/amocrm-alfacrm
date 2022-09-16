21e5f91af4dd92e4bbaf2d5e615b37659af972e4
cy322666 <cy322666@gmail.com>
2022-09-16 21:19:08 +0200

    change button amo

--- application/resources/views/buttons/amocrm/auth.blade.php
+++ application/resources/views/buttons/amocrm/auth.blade.php
@@ -3,9 +3,9 @@
         class="amocrm_oauth"
         charset="utf-8"
         data-client-id="{{ config('services.amocrm.client_id') }}"
-        data-name="{{ config('services.amocrm.app_name') }}"
-        data-description="{{ config('services.amocrm.description') }}"
-        data-redirect_uri="{{ config('services.amocrm.redirect_uri') }}"
+{{--        data-name="{{ config('services.amocrm.app_name') }}"--}}
+{{--        data-description="{{ config('services.amocrm.description') }}"--}}
+{{--        data-redirect_uri="{{ config('services.amocrm.redirect_uri') }}"--}}
         data-secrets_uri="{{ config('services.amocrm.secrets_uri') }}?account={{ \Illuminate\Support\Facades\Auth::user()->email }}"
         data-logo=""
         data-scopes="crm"
