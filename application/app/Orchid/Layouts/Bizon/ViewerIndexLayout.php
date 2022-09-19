f4be9a14b07b34dbeac7afc52c39c465b1b96240
cy322666 <cy322666@gmail.com>
2022-09-19 14:20:24 +0200

    hz

--- application/app/Orchid/Layouts/Bizon/ViewerIndexLayout.php
+++ application/app/Orchid/Layouts/Bizon/ViewerIndexLayout.php
@@ -23,10 +23,10 @@ class ViewerIndexLayout extends Table
         return 'Данных нет :(';
     }
 
-//    protected function striped(): bool
-//    {
-//        return false;
-//    }
+    protected function striped(): bool
+    {
+        return false;
+    }
     /**
      * @return string
      */
