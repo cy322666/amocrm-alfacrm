b1795b84f337634c6a3db107b77e7a9807de5ab6
cy322666 <cy322666@gmail.com>
2022-09-20 11:33:28 +0200

    fix speed site

--- application/resources/views/site/widgets.blade.php
+++ application/resources/views/site/widgets.blade.php
@@ -6,8 +6,8 @@
             </div>
         </div>
         <div class="col-md-6 mt-md-0 mt-6">
-            <div class="info-horizontal bg-gradient-disable border-radius-xl d-block d-md-flex p-6">
-                <h1 class="text-black">GetCourse</h1>
+            <div class="info-horizontal bg-gradient-primary border-radius-xl d-block d-md-flex p-6">
+                <h1 class="text-black">Bizon</h1>
             </div>
         </div>
         <div class="col-md-6 mt-md-3 mt-6">
@@ -17,7 +17,7 @@
         </div>
         <div class="col-md-6 mt-md-3 mt-6">
             <div class="info-horizontal bg-gradient-disable border-radius-xl d-block d-md-flex p-6">
-                <h1 class="text-black">YClients</h1>
+                <h1 class="text-black">GetCourse</h1>
             </div>
         </div>
     </div>
