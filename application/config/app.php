d70037041b744b662607f324ed10a066d534e55b
cy322666 <cy322666@gmail.com>
2022-09-26 19:01:36 +0200

    fix octane

--- application/config/app.php
+++ application/config/app.php
@@ -196,7 +196,6 @@ return [
         App\Providers\RouteServiceProvider::class,
 //        App\Providers\TelescopeServiceProvider::class,
 
-        \App\Providers\ClientsServiceProvider::class,
 
     ],
 
