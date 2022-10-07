8ccd03fc05d4288202da95e0e879f35ef177e9dc
cy322666 <cy322666@gmail.com>
2022-10-07 13:58:49 +0200

    font fix

--- application/resources/views/site/header.blade.php
+++ application/resources/views/site/header.blade.php
@@ -4,8 +4,8 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-7 pt-3 my-3 text-center mx-auto">
-                    <h1 class="text-black pt-3 mt-n5" style="font-size: 5rem; color: black">BLACK CLEVER</h1>
-                    <h2 class="text-black pt-3 mt-n5" style="font-size: 3rem; color: red" >Я - DIGITAL, А ТЫ?</h2>
+                    <h1 class="text-black pt-3 mt-n5" style="font-size: 5rem; color: black; font-family: Avenir">BLACK CLEVER</h1>
+                    <h2 class="text-black pt-3 mt-n5" style="font-size: 3rem; color: red; font-family: Avenir" >Я - DIGITAL, А ТЫ?</h2>
                 </div>
                 <div class="col-lg-7 pb-6 text-center mx-auto">
                     <p class="lead text-black mt-3">В наше неспокойное время DIGITAL`изации, автоматизация бизнеса становится не просто преимуществом, а средством выживания компании. Мы в этом помогаем</p>
